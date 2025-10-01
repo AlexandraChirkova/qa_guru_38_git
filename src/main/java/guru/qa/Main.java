a1068d5e644b7f9707b551a8e4c42c5c96f7d8a1
AlexandraChirkova <34439680+AlexandraChirkova@users.noreply.github.com>
2025-10-01 21:26:43 +0500

    Update Main.java

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -1,6 +1,9 @@
 package guru.qa;
 
 public class Main {
+
+    public static String msg = "Hello World";
+    
     public static void main(String[] args) {
         System.out.println("Hello Sasha!");
     }
